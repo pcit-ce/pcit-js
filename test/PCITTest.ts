db8bfa3239fde30d53632369ae0a32f434a6c739
khs1994 <khs1994@khs1994.com>
2019-01-17 12:00:57 +0800

    Update test

--- test/PCITTest.ts
+++ test/PCITTest.ts
@@ -1,4 +1,5 @@
-const ENTRYPOINT: string = 'https://ci2.khs1994.com:10000/api';
+const ENTRYPOINT: string =
+  process.env.NODE_PCIT_ENTRYPOINT || 'https://ci2.khs1994.com:10000/api';
 const TOKEN: string | undefined = process.env.NODE_PCIT_TOKEN;
 
 import PCIT from '../';
